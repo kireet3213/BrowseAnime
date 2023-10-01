3ac5e600892a53b6cdedc8f114dcd14f63a12991
kireet3213 <kireetjoshi1996@gmail.com>
2023-10-01 17:37:02 +0530

    hide delimiters

--- src/layouts/BaseCarouselComponent.vue
+++ src/layouts/BaseCarouselComponent.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-carousel cycle color="white" interval="3000" :show-arrows="true">
+  <v-carousel cycle color="white" interval="3000" height=670 :show-arrows="true" hide-delimiters>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
       :src="getImageUrl(item.src)"
       cover
-      max-height:300px
+      max-height:400px
     >
 
   </v-carousel-item>
